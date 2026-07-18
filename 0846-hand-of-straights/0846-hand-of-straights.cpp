ffb0282eb251039212375a06af97b1a84db1941a
Deepankar Singh <deepankarsingh106@gmail.com>
2026-07-18 15:26:36 +0530

    Time: 43 ms (37.13%), Space: 32.5 MB (22.43%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        int n = hand.size();
+        //storing the freqency 
+        map<int,int> freq;
+
+        for(int i:hand){
+            freq[i]++;
+        }
+
+        auto it = freq.begin(); // pointer it is 
+        while(it != freq.end()){
+
+            if(it->second == 0){
+                ++it;
+                continue;
+            }
+
+            auto start = it->first;
+            // minimum count of the element i needed 
+            // whenever next ot its element is there and 
+            // otherwise diffrence led to > 1
+            auto count = it->second;
+
+            for(int i = 0;i<groupSize;i++){
+
+                if(freq[i+start] < count)  return false;
+
+                freq[i+start] -= count;
+            }
+            it++;
+        }
+    return true;}
+};
