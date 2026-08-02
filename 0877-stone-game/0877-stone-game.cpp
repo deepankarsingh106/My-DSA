63f258e2cc091415ca078a67af0a2b70aabb6933
Deepankar Singh <deepankarsingh106@gmail.com>
2026-08-02 20:47:05 +0530

    Time: 0 ms (100%), Space: 10.5 MB (68.24%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+    //     int n = piles.size();
+    //     int alice = 0,bob = 0;
+    //     int i = 0,j=n-1;
+    //     int p = 0;
+    //     while(i <= j){
+    //         int choosen = max(piles[i],piles[j]);
+    //         if(choosen == piles[i]){
+    //             i++;
+    //         }
+    //         else{
+    //             j--;
+    //         }
+    //         if(p%2 == 0){
+    //             alice += choosen;
+    //             p++;
+    //         }
+    //         else{
+    //             bob += choosen;
+    //             p++;
+    //         }
+    //     }
+    // return alice > bob;}
+    return true;}
+};
