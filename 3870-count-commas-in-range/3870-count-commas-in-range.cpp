a6b2d1c637105ebf6a80097781fe953e422e5d75
Deepankar Singh <deepankarsingh106@gmail.com>
2026-09-09 00:21:49 +0530

    Time: 0 ms (100%), Space: 8.6 MB (51.38%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        int c = 0;
+        if(n < 1000){
+            return 0;
+        }
+        for(int i = 1000;i<=n;i++){
+            if(i <= 100000){
+                c++;
+            }
+        
+        }
+    return c;}
+};
