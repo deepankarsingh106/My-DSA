9b0aa14b9fc46d100b2d4cc84829195919855b96
Deepankar Singh <deepankarsingh106@gmail.com>
2026-09-15 23:09:04 +0530

    Time: 0 ms (100%), Space: 8.9 MB (91.36%) - LeetHub

---
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxPalindromes(string s, int k) {
+        
+        int n = s.size();
+        if(k == 1){
+            return n;
+        }
+        int c = 0;
+
+        for(int i = 0;i<=n-k;){
+            int step = 1;
+            for(int d : {k,k+1}){
+                if(i+d <= n && equal(s.begin()+i,s.begin()+i+(d >> 1), s.rbegin()+ (n-(i+d)))){
+                    step = d;
+                    c++;break;
+                }
+            }
+
+            i += step;
+        }
+    return c;}
+};
